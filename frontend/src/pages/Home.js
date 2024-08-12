4812c7f7394e20a9ea2f45e8c68770d136b8d598
obada kraishan <obada.kraishan@gmail.com>
2024-08-12 14:31:11 -0500

    AuthContext with the login and register

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventList from '../components/EventList';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Upcoming Events</h1>
+      <EventList />
+    </div>
+  );
+};
+
+export default Home;
