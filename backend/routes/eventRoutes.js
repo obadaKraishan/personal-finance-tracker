54f07a22078ef2c50a6bf5486ae72a464bc297d5
obada kraishan <obada.kraishan@gmail.com>
2024-08-13 17:06:19 -0500

    Routes (Update, Delete) Events updating

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createEvent, getEvents } = require('../controllers/eventController');
+const { createEvent, getEvents, updateEvent, deleteEvent } = require('../controllers/eventController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/create', authMiddleware, createEvent); // Route for creating events
 router.get('/', getEvents); // Route for fetching events
+router.put('/:id', authMiddleware, updateEvent); // Route for updating events
+router.delete('/:id', authMiddleware, deleteEvent); // Route for deleting events
 
 module.exports = router;
